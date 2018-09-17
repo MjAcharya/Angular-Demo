e36c514f5b1f64c9c2b2fbba4025b80dbf62a317
Manoj Acharya <mjacharya01@gmail.com>
2018-09-18 01:06:32 +0530

    removed the the json format list and added the list into the services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { ProductListComponent } from './products/product-list.component'
+import { ProductService } from "./products/product.service";
 
 @Component({
 selector:'pm-app',
-templateUrl:'app.component.html'
-
+templateUrl:'app.component.html',
+providers:[ProductService]
 })
 export class AppComponent{
   pageTitle:string ='Hello world!!!!';
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,38 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product"
+import { ProductService } from "./product.service";
 @Component({
     selector: 'pm-product',
     templateUrl: 'product-list.component.html',
     styleUrls: ['product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+    ngOnInit(): void {
+        this.products=this._productService.getProduct();
+    }
     productTitle: string = "Product List!!";
     imageWidth: number = 120;
     imageMargin: number = 2;
     listFilter: string;
     setButton: boolean = false;
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden  Cart",
-            "productCode": "GDN-909",
-            "releaseDate": "Dec 10 1995",
-            "description": "Coolllll!!!!",
-            "price": 34000,
-            "starRating": 3,
-            "imageUrl": "https://openclipart.org/image/2400px/svg_to_png/280962/ladyinreddances.png"
-        },
-        {
-            "productId": 3,
-            "productName": "Demon Gate",
-            "productCode": "GDN-909",
-            "releaseDate": "Dec 10 1995",
-            "description": "Coolllll!!!!",
-            "price": 5700,
-            "starRating": 2,
-            "imageUrl": "https://openclipart.org/image/90px/svg_to_png/290901/pixeldemon.png"
-        }
-    ];
+    products: IProduct[];
+
+    constructor(private _productService:ProductService){
+    }
 
     buttonToggle(): void {
         this.setButton = !this.setButton;
