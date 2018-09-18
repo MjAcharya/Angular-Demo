6082c2f0e182cd7bea45e6c19886cb2046a48fbf
Manoj Acharya <mjacharya01@gmail.com>
2018-09-18 23:15:50 +0530

    add the link to the product name

--- src/app/products/product-details.component.ts
+++ src/app/products/product-details.component.ts
@@ -1,10 +1,24 @@
 import { IProduct } from "./product";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "../../../node_modules/@angular/router";
 
 @Component({
-    templateUrl:'product-details.component.html'
+    templateUrl: 'product-details.component.html'
 })
-export class ProductDetailsComponent{
+export class ProductDetailsComponent implements OnInit {
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += ': ' + id;
+    }
     product: IProduct;
-    productTitle: string = 'Hello world!!!';
+    pageTitle: string = 'Product Detail';
+
+    constructor(private _route: ActivatedRoute,
+        private _router: Router) {
+
+    }
+
+    onBack(): void{
+        this._router.navigate(['/products']);
+    }
 }
