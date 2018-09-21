b7a273555ce0c98705c8d4c3070d899b5365f06a
Manoj Acharya <manoj.acharya@automationedge.com>
2018-09-21 13:03:06 +0530

    added the feature for individual product details

--- src/app/products/product-details.component.ts
+++ src/app/products/product-details.component.ts
@@ -1,24 +1,53 @@
 import { IProduct } from "./product";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterContentInit } from "@angular/core";
 import { ActivatedRoute, Router } from "../../../node_modules/@angular/router";
+import { ProductService } from "./product.service";
 
 @Component({
     templateUrl: 'product-details.component.html'
 })
 export class ProductDetailsComponent implements OnInit {
-    ngOnInit(): void {
-        let id = +this._route.snapshot.params['id'];
-        this.pageTitle += ': ' + id;
-    }
+
+    products: IProduct[];
     product: IProduct;
-    pageTitle: string = 'Product Detail';
+    id: number;
+    pageTitle: string = 'Product Detail'
+
+    ngOnInit(): void {
+
+        let othis = this;
+        othis.id = +othis._route.snapshot.params['id'];
+        othis.pageTitle += ': ' + othis.id;
+        othis.productList.getProduct().subscribe(res => {
+            othis.products = res;
+            for (let pro of othis.products) {
+                if (pro.productId === othis.id) {
+                    othis.product = pro;
+                }
+            }
+        });
+        this.list();
+    }
+
+
+
+
+    list(): void {
+
+        if (this.products)
+            console.log(this.products)
+        else
+            setTimeout(() => {
+                this.list();
+            }, 1000);
+    }
 
     constructor(private _route: ActivatedRoute,
-        private _router: Router) {
+        private _router: Router, private productList: ProductService) {
 
     }
 
-    onBack(): void{
+    onBack(): void {
         this._router.navigate(['/products']);
     }
 }
